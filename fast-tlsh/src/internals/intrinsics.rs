107f4a8366ccef1fb7849f2c0b5edd508eaa21ad
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2025-10-16 03:47:53 +0000

    intrinsics: Start using `core::hint::{likely,unlikely}()`
    Since `likely()` and `unlikely()` are no longer core intrinsics intended
    to be an internal feature of Rust itself, this commit starts using
    them as regular unstable functions.
    
    It also makes those functions constant and argument names simple.

--- fast-tlsh/src/internals/intrinsics.rs
+++ fast-tlsh/src/internals/intrinsics.rs
@@ -3,15 +3,17 @@
 
 //! The internal intrinsics.
 
+use cfg_if::cfg_if;
+
 /// Hints to the compiler that branch condition is likely to be true.
 ///
-/// This is a thin wrapper to [`core::intrinsics::likely()`] and requires
-/// `#![feature(core_intrinsics)]` when the `unstable` feature is enabled.
+/// This is a thin wrapper to [`core::hint::likely()`] and requires
+/// `#![feature(likely_unlikely)]` when the `unstable` feature is enabled.
 #[inline(always)]
 pub(crate) fn likely(value_likely_to_be_true: bool) -> bool {
-    cfg_if::cfg_if! {
+    cfg_if! {
         if #[cfg(feature = "unstable")] {
-            core::intrinsics::likely(value_likely_to_be_true)
+            core::hint::likely(value_likely_to_be_true)
         }
         else {
             value_likely_to_be_true
@@ -21,13 +23,13 @@ pub(crate) fn likely(value_likely_to_be_true: bool) -> bool {
 
 /// Hints to the compiler that branch condition is unlikely to be true.
 ///
-/// This is a thin wrapper to [`core::intrinsics::unlikely()`] and requires
-/// `#![feature(core_intrinsics)]` when the `unstable` feature is enabled.
+/// This is a thin wrapper to [`core::hint::unlikely()`] and requires
+/// `#![feature(likely_unlikely)]` when the `unstable` feature is enabled.
 #[inline(always)]
 pub(crate) fn unlikely(value_unlikely_to_be_true: bool) -> bool {
-    cfg_if::cfg_if! {
+    cfg_if! {
         if #[cfg(feature = "unstable")] {
-            core::intrinsics::unlikely(value_unlikely_to_be_true)
+            core::hint::unlikely(value_unlikely_to_be_true)
         }
         else {
             value_unlikely_to_be_true
