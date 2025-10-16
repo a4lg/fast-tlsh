f732196458ba0caa95be3c9b11735b399630e1e0
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2025-10-16 03:47:54 +0000

    bucket_aggregation: Update copyright format for fuzzer

--- fast-tlsh/src/internals/generate/bucket_aggregation/fuzzer.rs
+++ fast-tlsh/src/internals/generate/bucket_aggregation/fuzzer.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
-// SPDX-FileCopyrightText: Copyright (C) 2024–2025 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
+// SPDX-FileCopyrightText: Copyright (C) 2024, 2025 Tsukasa OI <floss_ssdeep@irq.a4lg.com>.
 
 use rand::{Rng, SeedableRng};
 use rand_xoshiro::Xoshiro256PlusPlus;
